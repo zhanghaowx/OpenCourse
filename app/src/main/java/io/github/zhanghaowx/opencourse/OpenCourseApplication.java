7770ce97799526fedcadd5dfdfd2f5c5f7da5a4f
Hao Zhang <zhanghaowx@gmail.com>
2015-03-14 20:29:13 -0500

    Add parse sdk

--- app/src/main/java/io/github/zhanghaowx/opencourse/OpenCourseApplication.java
+++ app/src/main/java/io/github/zhanghaowx/opencourse/OpenCourseApplication.java
@@ -2,7 +2,8 @@ package io.github.zhanghaowx.opencourse;
 
 import android.app.Application;
 
-import io.github.zhanghaowx.opencourse.utils.FontsOverride;
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 /**
  * The OpenCourse application
@@ -13,7 +14,22 @@ public final class OpenCourseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        startParseService();
+
+    }
+
+    private void setDefaultFont() {
         // Override default font
         //FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/.ttf");
+    }
+
+    private void startParseService() {
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "dIfjPyWxb27y8bOsUyRrQBDeTQOatdLtuiR0pgvT", "3xXJCl8Uqg1LvtnvI6Ky3qpXxUH6RZCYJ0THvHob");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 }
